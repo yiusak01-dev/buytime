9ad02a99c27c39f3cfb3e82180fbc7fdc6cf7548
買時間 <yiusak01@gmail.com>
2026-08-28 19:46:00 +0800

    fix: export startInstance + errorMiddleware

--- src/start.ts
+++ src/start.ts
@@ -3,7 +3,7 @@ import { createRouter } from "./router";
 import { supabaseAuthMiddleware } from "./integrations/supabase/auth-middleware";
 import { errorMiddleware } from "./lib/error-capture";
 
-export default createStartHandler({
+export const startInstance = createStartHandler({
   createRouter,
   middleware: [supabaseAuthMiddleware, errorMiddleware],
 })(defaultStreamHandler);
